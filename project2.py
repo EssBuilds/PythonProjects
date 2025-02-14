7a90554777ad42535d081c9591ae72e47586910f
Esther <esywacu@gmail.com>
2025-02-14 22:01:44 +0000

    added function

--- project2.py
+++ project2.py
@@ -1,17 +1,33 @@
-operand = input("Number 1: ")
-operand2 = input("Number 2: ")
-sign = input("Sign: ")
+def get_number(number):
+    while True:
+        operand = input("Number " + str(number) + ": " )
+        try:
+            return float(operand)
+        except:
+            print("Invalid number, try again.")
 
+
+   
+operand = get_number(1)
+operand2 = get_number(2)
+sign = input("sign: ")         
+
+result = 0
 if sign == "+":
-    result = float(operand) + float(operand2)
+            result = (operand) + (operand2)
 elif sign == "-":
-    result = float(operand) - float(operand2)
+            result = (operand) - (operand2)
 elif sign == "/":
-    if float(operand2) != 0:
-        result = float(operand) / float(operand2)
-    else:
-        print("Division by zero.")
+            if (operand2) != 0:
+                result = (operand) / (operand2)
+            else:
+                print("Division by zero.")
 elif sign == "*":
-    result = float(operand) * float(operand2)
+            result = (operand) * (operand2)
+else:
+        print("Invalid sign.")
+print(result) 
 
-print(result)
+
+
+
