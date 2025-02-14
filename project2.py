83835f20f274687f02e1b2058d021f93e901878f
Esther <esywacu@gmail.com>
2025-02-14 20:47:05 +0000

    added dividion by zero

--- project2.py
+++ project2.py
@@ -7,7 +7,10 @@ if sign == "+":
 elif sign == "-":
     result = float(operand) - float(operand2)
 elif sign == "/":
-    result = float(operand) / float(operand2)
+    if float(operand2) != 0:
+        result = float(operand) / float(operand2)
+    else:
+        print("Division by zero.")
 elif sign == "*":
     result = float(operand) * float(operand2)
 
